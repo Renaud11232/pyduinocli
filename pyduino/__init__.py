ce2693351c83a3f593e01eade9f5fc92f441ad3f
Renaud Gaspard <gaspardrenaud@hotmail.com>
2019-01-25 13:16:11 +0100

    added empty functions

--- pyduino/__init__.py
+++ pyduino/__init__.py
@@ -16,5 +16,74 @@ class Arduino:
             raise RuntimeError(stderr)
         return stdout
 
+    def board_attach(self):
+        pass
+
+    def board_details(self):
+        pass
+
+    def board_list(self):
+        pass
+
+    def board_listall(self):
+        pass
+
+    def compile(self):
+        pass
+
+    def config_dump(self):
+        pass
+
+    def config_init(self):
+        pass
+
+    def core_download(self):
+        pass
+
+    def core_install(self):
+        pass
+
+    def core_list(self):
+        pass
+
+    def core_search(self):
+        pass
+
+    def core_uninstall(self):
+        pass
+
+    def core_update_index(self):
+        pass
+
+    def core_upgrade(self):
+        pass
+
+    def lib_download(self):
+        pass
+
+    def lib_install(self):
+        pass
+
+    def lib_list(self):
+        pass
+
+    def lib_search(self):
+        pass
+
+    def lib_uninstall(self):
+        pass
+
+    def lib_update_index(self):
+        pass
+
+    def lib_upgrade(self):
+        pass
+
+    def sketch_new(self):
+        pass
+
+    def upload(self):
+        pass
+
     def version(self):
         return json.loads(self.__exec("version"))
