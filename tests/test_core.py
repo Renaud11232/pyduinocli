238a4e2fa7b61ab018b32fcb7e49d9c6b1936c9e
Renaud Gaspard <gaspardrenaud@hotmail.com>
2019-10-11 16:59:52 +0200

    added tests for cores

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,10 +1,53 @@
 from . import *
+import warnings
 
 
 class TestCoreCommand(TestBase):
 
-    def test_something(self):
-        pass
+    @classmethod
+    def setUpClass(cls):
+        cls._arduino.core.update_index()
+
+    def test_download(self):
+        self._arduino.core.download(["arduino:avr"])
+        warnings.warn("There is no return value to check, just checking it doesn't crash")
+
+    def test_install(self):
+        installed = self._arduino.core.list()
+        self.assertFalse(any(core["ID"] == "arduino:avr" for core in installed))
+        self._arduino.core.install(["arduino:avr"])
+        installed = self._arduino.core.list()
+        self.assertTrue(any(core["ID"] == "arduino:avr" for core in installed))
+        self._arduino.core.uninstall(["arduino:avr"])
+
+    def test_list(self):
+        installed = self._arduino.core.list()
+        self.assertIsInstance(installed, list)
+        self.assertFalse(any(core["ID"] == "arduino:avr" for core in installed))
+        self._arduino.core.install(["arduino:avr"])
+        installed = self._arduino.core.list()
+        self.assertIsInstance(installed, list)
+        self.assertTrue(any(core["ID"] == "arduino:avr" for core in installed))
+        self._arduino.core.uninstall(["arduino:avr"])
+
+    def test_search(self):
+        results = self._arduino.core.search(["avr"])
+        self.assertIsInstance(results, list)
+        self.assertTrue(any(core["ID"] == "arduino:avr" for core in results))
+
+    def test_uninstall(self):
+        self._arduino.core.install(["arduino:avr"])
+        self._arduino.core.uninstall(["arduino:avr"])
+        installed = self._arduino.core.list()
+        self.assertFalse(any(core["ID"] == "arduino:avr" for core in installed))
+
+    def test_update_index(self):
+        self._arduino.core.update_index()
+        warnings.warn("There is no return value to check, just checking it doesn't crash")
+
+    def test_upgrade(self):
+        self._arduino.core.upgrade()
+        warnings.warn("There is no return value to check, just checking it doesn't crash")
 
 
 if __name__ == '__main__':
