b2203ead1afd0b9f57a316625e6040e53d8b6e61
Renaud Gaspard <gaspardrenaud@hotmail.com>
2019-11-12 14:17:01 +0100

    release 0.4.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_long_description():
 
 setuptools.setup(
     name="pyduinocli",
-    version="0.4.0rc1",
+    version="0.4.0",
     author="Renaud Gaspard",
     author_email="gaspardrenaud@hotmail.com",
     description="Wrapper library around arduino-cli",
