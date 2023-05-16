326a3333ea1eb5359ba8e8781529f0787b38f7d3
Renaud Gaspard <gaspardrenaud@hotmail.com>
2023-05-16 18:09:23 +0200

    fixed update command

--- tests/test_update.py
+++ tests/test_update.py
@@ -6,8 +6,7 @@ class TestUpdateCommand(TestBase):
     def test_update(self):
         self._arduino.lib.install(["FastLED@3.3.2"])
         outdated = self._arduino.update(show_outdated=True)["result"]
-        self.assertIn("FastLED", outdated)
-        self.assertIn("3.3.2", outdated)
+        self.assertTrue(any(lib["library"]["name"] == "FastLED" and lib["library"]["version"] == "3.3.2" for lib in outdated["libraries"]))
         self._arduino.lib.uninstall(["FastLED@3.3.2"])
 
 
